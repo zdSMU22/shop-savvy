9746bd9aa4eb694265d7f95515f62a836715edbf
Dixon <zdixon91.zd@gmail.com>
2022-08-20 01:47:02 -0500

    column updated for tags

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,16 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    tag_name:{
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
